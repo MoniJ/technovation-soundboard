4eb4aeb814f5894dcf2339dd663abb6e7ae25fb9
Monica Jimenez <monij_93@hotmail.com>
2025-02-26 21:49:47 +0000

    Changes to support soundboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+def click_func(col):
+    speechContainer.audio(speeches[col], format="audio/mpeg")
+
+st.title("Soundboard App")
+st.subheader(
+    "Click on a person's name to hear the speech"
 )
+
+
+names = ["Kamala", "Martin", "Brown"]
+images = ["assets/kamalaharris.png", "assets/mlk.jpg", "assets/brownjackson.jpg"]
+speeches = ["assets/kamala.mp3", "assets/mlk.mp3", "assets/brownjackson.mp3"]
+
+cols = st.columns(len(names))
+
+for a in range(0, len(names)):
+    cols[a].image(images[a])
+    cols[a].button(names[a], on_click=click_func, args=(a,), use_container_width=True)
+
+speechContainer = st.container(border=True)
+speechContainer.write("Audio: ")
